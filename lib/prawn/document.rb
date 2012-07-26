cf8dfca98e352a958b2ba1b13af3288758e96617
Amant Stephane <stephane.amant@belighted.com>
2012-07-26 15:46:13 +0200

    Concat a complete PDF file to the current document - check if file exists

--- lib/prawn/document.rb
+++ lib/prawn/document.rb
@@ -377,9 +377,11 @@ module Prawn
     #  pdf.concat("foo.pdf")
     #
     def concat(pdf_file)
-      pdf_temp_nb_pages = Prawn::Document.new(:template => pdf_file).page_count
-      (1..pdf_temp_nb_pages).each do |i|
-        self.start_new_page(:template => pdf_file, :template_page => i)
+      if File.exists?(pdf_file)
+        pdf_temp_nb_pages = Prawn::Document.new(:template => pdf_file).page_count
+        (1..pdf_temp_nb_pages).each do |i|
+          self.start_new_page(:template => pdf_file, :template_page => i)
+        end
       end
     end
 
